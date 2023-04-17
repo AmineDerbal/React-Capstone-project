c106ec353bbea3068f7ca97ce7b720a0b333cc51
Amine derbal <aminedoran31@gmail.com>
2023-04-17 15:18:50 +0100

    add navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faMicrophone, faGear } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faMicrophone } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
-const Header = () => (
-  <header>
-    <div className="navigation-left">
-      <FontAwesomeIcon icon={faChevronLeft} style={{ color: '#fff' }} />
-      <span>2023</span>
-    </div>
-    <h1>Coins States</h1>
-    <div className="navigation-right">
-      <FontAwesomeIcon icon={faMicrophone} />
-      <FontAwesomeIcon icon={faGear} />
-    </div>
-  </header>
-);
+const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <header>
+      <div className="navigation-left">
+        <FontAwesomeIcon
+          className="chevron-left"
+          onClick={() => {
+            navigate('/');
+          }}
+          icon={faChevronLeft}
+          style={{ color: '#fff' }}
+        />
+        <span>2023</span>
+      </div>
+      <h1>Coins States</h1>
+      <div className="navigation-right">
+        <FontAwesomeIcon icon={faMicrophone} />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
