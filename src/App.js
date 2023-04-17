70e2918065d5ffbd6013d2bae56541b9c1839a03
Amine derbal <aminedoran31@gmail.com>
2023-04-17 13:40:46 +0100

    add path for details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './routes/Home';
+import Details from './routes/Details';
 import './App.css';
 
 const App = () => (
@@ -11,6 +12,7 @@ const App = () => (
     </div>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/details/:id" element={<Details />} />
     </Routes>
   </Router>
 );
