98a37b8a7bdcae7f3e1b2e9d999484924d37e37a
Amine derbal <aminedoran31@gmail.com>
2023-04-17 13:42:01 +0100

    import detail reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cryptosReducer from './crypto/cryptoSlice';
+import detailReducer from './detail/detailSlice';
 
 const store = configureStore({
   reducer: {
     crypto: cryptosReducer,
+    detail: detailReducer,
   },
 });
 
