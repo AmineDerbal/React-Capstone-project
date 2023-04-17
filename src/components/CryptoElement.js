a06b27f6df3dea58a4a60b32bc3bb5213ec0f7ac
Amine derbal <aminedoran31@gmail.com>
2023-04-17 13:43:25 +0100

    add navigation for crypto element

--- src/components/CryptoElement.js
+++ src/components/CryptoElement.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/cryptoElement.css';
+import { useNavigate } from 'react-router-dom';
 
-const CryptoElement = ({ crypto, index }) => (
-  <div className="crypto-card">
-    <h2>
-      {crypto.id}
-      {index}
-    </h2>
-    <img className="crypto-icon" src={crypto.icon} alt={crypto.id} />
-  </div>
-);
+const CryptoElement = ({ crypto, index }) => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="crypto-card"
+      onClick={() => navigate(`/details/${crypto.id}`)}
+      aria-hidden="true"
+    >
+      <h2>
+        {crypto.id}
+        {index}
+      </h2>
+      <img className="crypto-icon" src={crypto.icon} alt={crypto.id} />
+    </div>
+  );
+};
 
 CryptoElement.propTypes = {
   crypto: PropTypes.shape({
