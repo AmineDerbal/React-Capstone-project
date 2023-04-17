7c846932a06b49399d196266dda5d8d869c42191
Amine derbal <aminedoran31@gmail.com>
2023-04-17 11:05:02 +0100

    import styleing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './routes/Home';
+import './App.css';
 
 const App = () => (
   <Router>
