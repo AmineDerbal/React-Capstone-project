213b362afce14864e450996efd28b8cd6a1585d0
Amine derbal <aminedoran31@gmail.com>
2023-04-17 19:04:12 +0100

    fix error linter

--- src/components/DetailElement.js
+++ src/components/DetailElement.js
@@ -55,32 +55,34 @@ const DetailElement = ({ details }) => {
               <h3>Options</h3>
             </div>
             <div className="font-size-container">
-              <label htmlFor="font-size">Font size</label>
-              <select
-                onChange={(e) => {
-                  setIsFontSize(Number(e.target.value));
-                }}
-                id="font-size"
-                name="font-size"
-              >
-                <option value="">--Please choose an option--</option>
-                <option value="16">16 </option>
-                <option value="17">17 </option>
-                <option value="18">18 </option>
-                <option value="19">19 </option>
-                <option value="20">20 </option>
-                <option value="21">21 </option>
-                <option value="22">22 </option>
-                <option value="22">22 </option>
-                <option value="23">23 </option>
-                <option value="24">24 </option>
-                <option value="25">25 </option>
-                <option value="26">26 </option>
-                <option value="27">27 </option>
-                <option value="28">28 </option>
-                <option value="29">29 </option>
-                <option value="30">30 </option>
-              </select>
+              <label htmlFor="font-size">
+                Font size
+                <select
+                  onChange={(e) => {
+                    setIsFontSize(Number(e.target.value));
+                  }}
+                  id="font-size"
+                  name="font-size"
+                >
+                  <option value="">--Please choose an option--</option>
+                  <option value="16">16 </option>
+                  <option value="17">17 </option>
+                  <option value="18">18 </option>
+                  <option value="19">19 </option>
+                  <option value="20">20 </option>
+                  <option value="21">21 </option>
+                  <option value="22">22 </option>
+                  <option value="22">22 </option>
+                  <option value="23">23 </option>
+                  <option value="24">24 </option>
+                  <option value="25">25 </option>
+                  <option value="26">26 </option>
+                  <option value="27">27 </option>
+                  <option value="28">28 </option>
+                  <option value="29">29 </option>
+                  <option value="30">30 </option>
+                </select>
+              </label>
             </div>
           </div>
         </div>
