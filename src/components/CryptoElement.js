4f1893c1228844a5bbbe379a46ec557c7b4282b8
Amine derbal <aminedoran31@gmail.com>
2023-04-17 11:06:25 +0100

    update display

--- src/components/CryptoElement.js
+++ src/components/CryptoElement.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/cryptoElement.css';
 
-const CryptoElement = ({ crypto }) => (
+const CryptoElement = ({ crypto, index }) => (
   <div className="crypto-card">
-    <h2>{crypto.id}</h2>
+    <h2>
+      {crypto.id}
+      {index}
+    </h2>
+    <img className="crypto-icon" src={crypto.icon} alt={crypto.id} />
   </div>
 );
 
 CryptoElement.propTypes = {
   crypto: PropTypes.shape({
     id: PropTypes.string,
+    icon: PropTypes.string,
   }).isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default CryptoElement;
