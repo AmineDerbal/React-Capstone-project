697db9884bc628bfdb656f696fe7721f6bba1361
Amine derbal <aminedoran31@gmail.com>
2023-04-17 11:04:44 +0100

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faMicrophone, faGear } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => (
   <header>
-    <h2>Hello</h2>
+    <div className="navigation-left">
+      <FontAwesomeIcon icon={faChevronLeft} style={{ color: '#fff' }} />
+      <span>2023</span>
+    </div>
+    <h1>Coins States</h1>
+    <div className="navigation-right">
+      <FontAwesomeIcon icon={faMicrophone} />
+      <FontAwesomeIcon icon={faGear} />
+    </div>
   </header>
 );
 
