1fd187238a8f21b66696cb8a1d7398d8c8a3b314
Amine derbal <aminedoran31@gmail.com>
2023-04-17 11:04:31 +0100

    update Home route

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,14 +2,9 @@ import React from 'react';
 import CryptoList from '../components/CryptoList';
 
 const Home = () => (
-  <div className="container">
-    <div className="bookstore-container">Hello there</div>
+  <>
     <CryptoList />
-    <div className="Line" />
-    <div className="form-container">
-      <h2 className="form-title">ADD NEW BOOK</h2>
-    </div>
-  </div>
+  </>
 );
 
 export default Home;
